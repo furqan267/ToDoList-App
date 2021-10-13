5ae4a4fac002a05f7b6b028057717fac0f69c758
Furqan <sfurqan068@gmail.com>
2021-10-13 19:38:07 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,81 @@
+// 1. Ways to print in Javascript
+// console.log("Hello World");
+// alert("me");
+// document.write("This is a Document Write");
+
+// 2. Javascript console API
+// console.log("Hello World");
+// console.warn("This is Warning");
+// console.error("This is an Error");
+
+
+/*
+multi
+line
+comment
+*/
+
+// 3. What are Variables? - Containers to store data values
+
+// var number1 = 56;
+// var number2 = 34;
+// console.log ( number1 +  number2);
+
+// 4. Data types 
+
+// var num1 = 56;
+// var num2 = 34.55;
+
+// 5. String
+
+// var str1 = "This is String";
+// var str2 = 'This is also string';
+
+// 6. Object
+
+// var marks = {
+//     ravi: 34,
+//     mayank: 56,
+//     sheru: 89,
+// }
+// console.log(marks);
+
+// 7. Booleans
+
+// var a = true;
+// var b = false;
+// console.log(a,b);
+
+/* 8. At very high level data types are of two types:
+   a. Primitive data type - number, string, boolean, null, undefined, symbol
+   b. Reference data type - arrays and objects
+*/ 
+
+// var arr = [1,2,3,5] 
+// console.log(arr)
+
+//  Operators in  javascript
+//  Arithmetic Operators
+
+// var a = 100;
+// var b = 10;
+
+// console.log("The value of a + b is", a+b);
+// console.log("The value of a - b is", a-b);
+// console.log("The value of a * b is", a*b);
+// console.log("The value of a / b is", a/b);
+
+
+// Array Methods
+// var myArr = ["Fan", "34", null, true ];
+// myArr.pop(null);
+// myArr.push("amit");
+// console.log(myArr);
+
+// DOM manipulation
+
+let elem = document.getElementById("click");
+console.log(elem);
+
+let elemClass = document.getElementsByClassName("container");
+console.log('elemClass');
